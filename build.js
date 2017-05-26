a09b52e03d59493bad2db03470d4b01b3efa1693
leo.liu@gaodun.com <leo.liu@gaodun.com>
2017-05-26 18:19:26 +0800

    save

--- build.js
+++ build.js
@@ -1,4 +1,13 @@
 var path=require('path');
+var Mock = require('mockjs')
+var data = Mock.mock(/\.json/,{
+    // 属性 list 的值是一个数组，其中含有 1 到 10 个元素
+    'list|1-10': [{
+        // 属性 id 是一个自增数，起始值为 1，每次增 1
+        'id|+1': 1
+    }]
+})
+console.log(JSON.stringify(data, null, 4))
 var [p1,p2,taskName,...fileNames] = process.argv;
 
 var moduleName = taskName.split(':')[0];
